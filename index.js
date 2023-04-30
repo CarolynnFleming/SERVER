937a8247d8fc034df8236edf2236d89d802e9482
CarolynnFleming <cfcatpink@gmail.com>
2023-04-30 16:53:16 -0500

    hmm

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ require('dotenv').config({ path: './.env'});
 const path = require('path');
 const app =express();
 const createCheckoutSession = require('./api//checkout')
-const port = 5050;
-
+// app.use(
+//     require('cors')({
+//       origin: ['https://jovial-choux-3e4c2c.netlify.app'],
+//       credentials: true,
+//     })
+//   );
 app.use(express.json());
 app.use(cors({ origin: true}));
 app.use(express.static(path.resolve(__dirname,"./client/build")));
 app.get('/', (req, res) => res.send('YO Peeps'));
 
 app.post('/create-checkout-session', createCheckoutSession);
-app.listen(port, () => console.log('server listening on port', port));
+app.listen(process.env.Port || 5050, () => console.log('server listening on port'));
